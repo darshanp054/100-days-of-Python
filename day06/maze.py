62cee0968b875bc93bd836cb131270923c31d811
Darshan Patel <87711310+darshannn10@users.noreply.github.com>
2022-12-08 15:44:52 +0530

    Update maze.py

--- day06/maze.py
+++ day06/maze.py
@@ -1,7 +1,3 @@
-'''
-Visit the link : https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
-'''
-
 def turn_right():
     turn_left()
     turn_left()
