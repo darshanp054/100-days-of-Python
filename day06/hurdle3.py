860b72690b76d1df3a5eeb6b9eac5bcd675be81f
Darshan Patel <87711310+darshannn10@users.noreply.github.com>
2022-12-08 15:45:32 +0530

    Update hurdle3.py

--- day06/hurdle3.py
+++ day06/hurdle3.py
@@ -1,7 +1,3 @@
-'''
-Visit the link : https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
-'''
-
 def turn_right():
     turn_left()
     turn_left()
